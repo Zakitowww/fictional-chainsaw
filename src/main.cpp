3b6bb628377652bddb5080bdb335eab451333104
Zekria Sormasor <zekria.sormasor@he-arc.ch>
2025-03-30 00:12:14 +0100

    feat: just the basic circle shape in the main file...

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,31 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        sf::CircleShape shape(50.f);
+        
+        // set the shape color to green
+        shape.setFillColor(sf::Color(100, 250, 50));
+        window.draw(shape);
+
+        // end the current frame
         window.display();
     }
 }
